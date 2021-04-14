20df7bb9fc6699edb5947e90e3660d1d48a43746
ashler24 <pawar2467@gmail.com>
2021-04-15 00:14:52 +0530

    added heroku node url

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: `http://localhost:3200/`
+    // baseURL: `http://localhost:3200/`
+    baseURL: `https://recipe-house-node.herokuapp.com/`
 });
